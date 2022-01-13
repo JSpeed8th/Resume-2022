efab2443af17fd77e482680b3494ff8ef4cf89c5
Jordan <jspeed8th@gmail.com>
2022-01-12 20:20:43 -0500

    Cut out sky and added mp4 of clouds.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,23 @@
 import '../styles/Hero.css'
 import {ReactComponent as ReactLogo} from '../assets/hero-name.svg';
+import skyVideo from '../assets/cloud-videos/Formation Of Clouds.mp4';
+
 
 const Hero = () => {
     return (
-        <section className='hero-background-image' id='hero'>
+        <section className='hero-background-image-wrapper' id='hero'>
+            <video className='hero-background-image-video' loop muted autoPlay playsinline width="100%" height="auto">
+                <source src={skyVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div className='hero-background-image'>
             {/* <img className='hero-foreground-image' src='' alt='' /> */}
                 <ReactLogo className='hero-logo' />
             <h1 className='hero-header visually-hidden'>
                 Jordan Speed's Portfolio Website
             </h1>
+            </div>
+
         </section>
     )
 }
